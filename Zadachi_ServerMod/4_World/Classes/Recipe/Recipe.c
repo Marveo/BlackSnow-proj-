0faaa74fb7969485f2ef2a20fe97ef6ebc8217f6
Marveo <144726854+Marveo@users.noreply.github.com>
2026-03-04 21:10:25 +0200

    Delete Zadachi_ServerMod directory

--- Zadachi_ServerMod/4_World/Classes/Recipe/Recipe.c
+++
@@ -1,9 +0,0 @@
-modded class RecipeBase
-{
-	override void SpawnItems(ItemBase ingredients[], PlayerBase player, array<ItemBase> spawned_objects/*out*/)
-	{
-		super.SpawnItems(ingredients, player, spawned_objects);
-
-		KTQuestOnEntityCrafted.Invoke(player, spawned_objects);
-	}
-}
--- Zadachi_ServerMod/4_World/Entities/ItemBase/PlantBase.c
+++
@@ -1,9 +0,0 @@
-modded class PlantBase
-{
-	override void Harvest( PlayerBase player )
-	{
-		super.Harvest(player);
-
-		KTQuestOnHarvest.Invoke(GetType(), player);
-	}
-}
