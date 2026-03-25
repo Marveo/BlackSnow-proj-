7194640f35566bfeccce14871779ebf5b4e5438d
Marveo <144726854+Marveo@users.noreply.github.com>
2026-03-25 21:42:18 +0200

    Delete arm directory

--- arm/PluginTransmissionAgents.c
+++
@@ -1,8 +0,0 @@
-modded class PluginTransmissionAgents extends PluginBase
-{	
-	void PluginTransmissionAgents()
-	{
-		m_AgentList.Insert(SkillAgent.AMMO_CRAFTING, new AmmoCraftingAgent);
-	}
-};
-
--- arm/SkillAgent.c
+++
@@ -1,4 +0,0 @@
-enum SkillAgent
-{
-	AMMO_CRAFTING = 2097152, // унікальний ID нашої навички
-}
